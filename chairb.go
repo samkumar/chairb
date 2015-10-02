326958b81543544ecd5413b0ccc70635dcb7371b
Sam Kumar <samkumar@berkeley.edu>
2015-10-01 22:11:03 -0700

    Basic subscription

--- chairb.go
+++ chairb.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"os"
+	
+	bw "github.com/immesys/bw2bind"
 )
 
+const (
+	RAWDATAURI = "castle.bw2.io/sam/test/rawdata"
+)
+
+func exitOnError(err error, msg string) {
+	if err != nil {
+		fmt.Printf("%s: %v\n", msg, err)
+		os.Exit(1)
+	}
+}
+
 func main() {
-	fmt.Println("Chair backend")
+	var err error
+	var cl *bw.BW2Client
+	
+	cl, err = bw.Connect("localhost:28589")
+	exitOnError(err, "Could not connect")
+	
+	var vk string
+	vk, err = cl.SetEntityFile("chairb.key")
+	exitOnError(err, "Could not use entity")
+	
+	var dchain *bw.SimpleChain
+	dchain, err = cl.BuildAnyChain(RAWDATAURI, "P", vk)
+	exitOnError(err, "Could not build DOT chain")
+	
+	var msgchan chan *bw.SimpleMessage
+	msgchan, err = cl.Subscribe(&bw.SubscribeParams{
+		URI: RAWDATAURI,
+		PrimaryAccessChain: dchain.Hash,
+	})
+	
+	var msg *bw.SimpleMessage
+	for msg = range msgchan {
+		msg.Dump()
+	}
 }
